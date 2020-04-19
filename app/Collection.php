134e7f275e8dea4704a643b37fbb3d0320bedee2
Derrick <derrick@solutech.co.ke>
2020-04-20 00:27:53 +0300

    mpesa changes

--- app/Collection.php
+++ app/Collection.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends Model
 {
-    protected $fillable = ['client_id', 'parking_spot_id', 'payment_type', 'merchantRequestId', 'checkoutRequestId', 'amount', 'balance', 'partyA', 'partyB', 'status', 'receipt_no'];
+    protected $fillable = ['vehicle_id', 'client_id', 'parking_spot_id', 'payment_type', 'merchantRequestId', 'checkoutRequestId', 'amount', 'ResultDesc', 'partyA', 'partyB', 'status', 'receipt_no'];
 
 
     public function client()
@@ -14,8 +14,9 @@ class Collection extends Model
         return $this->belongsTo(Client::class, 'client_id', 'id');
     }
 
-    public function parking_spot(){
-        return $this->belongsTo(ParkingSpot::class,'parking_spot_id','id');
+    public function parking_spot()
+    {
+        return $this->belongsTo(ParkingSpot::class, 'parking_spot_id', 'id');
     }
 
 }
---
+++ app/Jobs/PaymentStatusFail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use LaravelFCM\Facades\FCM;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use LaravelFCM\Message\Topics;
+
+class PaymentStatusFail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $message,$user;
+
+    /**
+     * PaymentStatusFail constructor.
+     * @param $message
+     * @param $user
+     */
+    public function __construct($message, $user)
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $notificationBuilder = new PayloadNotificationBuilder('Payment status');
+        $notificationBuilder->setBody($this->message)
+            ->setSound('default');
+
+        $notification = $notificationBuilder->build();
+        $topic = new Topics();
+        $topic->topic($this->user->id);
+        FCM::sendToTopic($topic, null, $notification, null);
+    }
+}
