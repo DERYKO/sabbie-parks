16a6f5a992fc82c7c9ccc0a33b905506fa6b9356
Derrick <derrick@solutech.co.ke>
2020-05-01 17:54:01 +0300

    few changes

--- app/Collection.php
+++ app/Collection.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends Model
 {
-    protected $fillable = ['vehicle_id', 'client_id', 'parking_spot_id', 'payment_type', 'merchantRequestId', 'checkoutRequestId', 'amount', 'ResultDesc', 'partyA', 'partyB', 'status', 'receipt_no'];
+    protected $fillable = ['user_vehicle_id', 'client_id', 'parking_spot_id', 'payment_type', 'merchantRequestId', 'checkoutRequestId', 'amount', 'ResultDesc', 'partyA', 'partyB', 'status', 'receipt_no'];
 
 
     public function client()
     {
         return $this->belongsTo(Client::class, 'client_id', 'id');
+    }
+    public function user_vehicle(){
+        return $this->belongsTo(UserVehicle::class,'user_vehicle_id','id');
     }
 
     public function parking_spot()
