6ea34b909e8aa5ac06bbe64ef47fb6ee9f3c5e1e
Derrick <derrick@solutech.co.ke>
2021-02-01 12:19:03 +0300

    maps key

--- nova/resources/views/layout.blade.php
+++ nova/resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i" rel="stylesheet">
+    <script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyBbMbpTY8u3RUhX8KUrvq0eSLHxkNZUp-k"></script>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('app.css', 'vendor/nova') }}">
