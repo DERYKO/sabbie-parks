7d2c371b77f6997b41551d0c7e9b8a246cd387a4
Derrick <derrick@solutech.co.ke>
2021-02-23 22:03:44 +0300

    format date

--- app/Wallet.php
+++ app/Wallet.php
@@ -2,13 +2,20 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
     protected $fillable = ['user_id', 'transaction_type', 'debit', 'credit', 'balance'];
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->toDayDateTimeString();
     }
 }
