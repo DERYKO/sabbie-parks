a4521894cae7e033ee8076508dbdbcaa79111d2c
Derrick <derrick@solutech.co.ke>
2022-02-27 12:24:57 +0300

    login user

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
         ]);
         $user = User::where('phone_number','like', '%'.$request->phone_number.'%')
             ->first(['id', 'code', 'first_name', 'last_name', 'phone_number', 'email']);
-        Log::info($user);
+        Log::info($user->code == $request->code);
         if ($user->code == $request->code) {
             $token = $user->createToken('MyApp')->accessToken;
             return response()->json(['user' => $user, 'token' => $token],200);
